01fe198dcf8056c0dcd54df8dfc6ccc4a3e961ff
Romeo Calota <mail@romeocalota.me>
2025-03-03 20:47:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿using System.Diagnostics;
+using Silk.NET.SDL;
+
+namespace TheAdventure;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var sdl = new Sdl(new SdlContext());
+
+        UInt64 framesRenderedCounter = 0;
+        var timer = new Stopwatch();
+
+        ReadOnlySpan<byte> keyboardState;
+        unsafe
+        {
+            keyboardState = new(sdl.GetKeyboardState(null), (int)KeyCode.Count);
+        }
+
+        Span<byte> mouseButtonStates = stackalloc byte[(int)MouseButton.Count];
+
+        var ev = new Event();
+
+        var sdlInitResult = sdl.Init(Sdl.InitVideo | Sdl.InitAudio | Sdl.InitEvents | Sdl.InitTimer | Sdl.InitGamecontroller |
+                                     Sdl.InitJoystick);
+        if (sdlInitResult < 0)
+        {
+            throw new InvalidOperationException("Failed to initialize SDL.");
+        }
+
+        IntPtr window;
+        unsafe
+        {
+            window = (IntPtr)sdl.CreateWindow(
+                "The Adventure", Sdl.WindowposUndefined, Sdl.WindowposUndefined, 800, 800,
+                (uint)WindowFlags.Resizable | (uint)WindowFlags.AllowHighdpi
+            );
+
+            if (window == IntPtr.Zero)
+            {
+                var ex = sdl.GetErrorAsException();
+                if (ex != null)
+                {
+                    throw ex;
+                }
+
+                throw new Exception("Failed to create window.");
+            }
+        }
+
+        IntPtr renderer;
+        unsafe
+        {
+            renderer = (IntPtr)sdl.CreateRenderer((Window*)window, -1, (uint)RendererFlags.Accelerated);
+        }
+
+        if (renderer == IntPtr.Zero)
+        {
+            var ex = sdl.GetErrorAsException();
+            if (ex != null)
+            {
+                throw ex;
+            }
+
+            throw new Exception("Failed to create renderer.");
+        }
+
+        bool quit = false;
+        while (!quit)
+        {
+            while (sdl.PollEvent(ref ev) != 0)
+            {
+                if (ev.Type == (uint)EventType.Quit)
+                {
+                    quit = true;
+                    break;
+                }
+
+                switch (ev.Type)
+                {
+                    case (uint)EventType.Windowevent:
+                    {
+                        switch (ev.Window.Event)
+                        {
+                            case (byte)WindowEventID.Shown:
+                            case (byte)WindowEventID.Exposed:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.Hidden:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.Moved:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.SizeChanged:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.Minimized:
+                            case (byte)WindowEventID.Maximized:
+                            case (byte)WindowEventID.Restored:
+                                break;
+                            case (byte)WindowEventID.Enter:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.Leave:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.FocusGained:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.FocusLost:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.Close:
+                            {
+                                break;
+                            }
+                            case (byte)WindowEventID.TakeFocus:
+                            {
+                                unsafe
+                                {
+                                    sdl.SetWindowInputFocus(sdl.GetWindowFromID(ev.Window.WindowID));
+                                }
+
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case (uint)EventType.Fingermotion:
+                    {
+                        break;
+                    }
+
+                    case (uint)EventType.Mousemotion:
+                    {
+                        break;
+                    }
+
+                    case (uint)EventType.Fingerdown:
+                    {
+                        mouseButtonStates[(byte)MouseButton.Primary] = 1;
+                        break;
+                    }
+                    case (uint)EventType.Mousebuttondown:
+                    {
+                        mouseButtonStates[ev.Button.Button] = 1;
+                        break;
+                    }
+
+                    case (uint)EventType.Fingerup:
+                    {
+                        mouseButtonStates[(byte)MouseButton.Primary] = 0;
+                        break;
+                    }
+
+                    case (uint)EventType.Mousebuttonup:
+                    {
+                        mouseButtonStates[ev.Button.Button] = 0;
+                        break;
+                    }
+
+                    case (uint)EventType.Mousewheel:
+                    {
+                        break;
+                    }
+
+                    case (uint)EventType.Keyup:
+                    {
+                        break;
+                    }
+
+                    case (uint)EventType.Keydown:
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var elapsed = timer.Elapsed;
+            timer.Restart();
+
+            // game.render(renderer, RenderEvent{ elapsed, framesRenderedCounter++ });
+            unsafe
+            {
+                sdl.RenderPresent((Renderer*)renderer);
+            }
+
+            ++framesRenderedCounter;
+        }
+
+        unsafe
+        {
+            sdl.DestroyWindow((Window*)window);
+        }
+
+        sdl.Quit();
+    }
+}
